adb1fd2c9166e19cbaceda9b01691fd4992b9d22
Kimwel Beller <94892701+Kheemwel@users.noreply.github.com>
2024-08-04 14:47:35 +0800

    refactor(ui): change month-year  dropdown ui - change indicator of current day with entry in calendar - add swiping function for calendar to navigate each months

--- lib/screens/home/calendar_view.dart
+++ lib/screens/home/calendar_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter_dtr_app/core/api/datetime_api.dart';
 import 'package:flutter_dtr_app/core/theme.dart';
 import 'package:flutter_dtr_app/data/models/daily_time_records_model.dart';
 import 'package:flutter_dtr_app/screens/home/add_entry.dart';
-import 'package:flutter_dtr_app/widgets/calendar_popup_menu_item.dart';
 import 'package:flutter_dtr_app/widgets/typography.dart';
 
 class CalendarView extends StatefulWidget {
@@ -14,7 +13,6 @@ class CalendarView extends StatefulWidget {
 }
 
 class _CalendarViewState extends State<CalendarView> {
-  late DateTime _currentDate = DateTime(_selectedYear, DateTime.now().month, 1);
   final List<String> _months = [
     'January',
     'February',
@@ -40,10 +38,9 @@ class _CalendarViewState extends State<CalendarView> {
     'SAT',
   ];
 
-  int _selectedYear = DateTime.now().year;
+  DateTime _currentDate = DateTime.now();
   final int _minimumYear = 2000;
   final int _maximumYear = DateTime.now().year;
-
   final DailyTimeRecordsModel _dailyTimeRecordsModel = DailyTimeRecordsModel();
 
   @override
@@ -85,44 +82,15 @@ class _CalendarViewState extends State<CalendarView> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        PopupMenuButton<String>(
-            surfaceTintColor: Colors.white,
-            tooltip: 'Select Month',
-            constraints: const BoxConstraints(maxHeight: 200),
-            position: PopupMenuPosition.under,
-            initialValue: _months[_currentDate.month - 1],
-            onSelected: (String month) {
+        _buildMonthYearDropDown(
+            selectedMonthYear: _currentDate,
+            minimumYear: _minimumYear,
+            maximumYear: _maximumYear,
+            onSelected: (DateTime date) {
               setState(() {
-                _currentDate = DateTime(_selectedYear, _months.indexOf(month) + 1, 1);
+                _currentDate = date;
               });
-            },
-            itemBuilder: (BuildContext context) => List.generate(
-                _months.length,
-                (month) => CalendarPopupMenuItem(
-                      value: _months[month],
-                      isSelected: month == _currentDate.month - 1,
-                    )),
-            child: buildHeading3Text(_months[_currentDate.month - 1])),
-        const SizedBox(
-          width: 5,
-        ),
-        PopupMenuButton<int>(
-            tooltip: 'Select Year',
-            constraints: const BoxConstraints(maxHeight: 200),
-            position: PopupMenuPosition.under,
-            initialValue: _selectedYear,
-            onSelected: (int year) {
-              setState(() {
-                _selectedYear = year;
-                _currentDate = DateTime(_selectedYear, _currentDate.month, 1);
-              });
-            },
-            itemBuilder: (BuildContext context) => List.generate(
-                    _maximumYear - _minimumYear + 1, (index) => _minimumYear + index)
-                .map(
-                    (year) => CalendarPopupMenuItem(value: year, isSelected: year == _selectedYear))
-                .toList(),
-            child: buildHeading3Text(_selectedYear.toString())),
+            }),
         const Spacer(),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -142,64 +110,224 @@ class _CalendarViewState extends State<CalendarView> {
     );
   }
 
+  Widget _buildMonthYearDropDown({
+    required DateTime selectedMonthYear,
+    required int minimumYear,
+    required int maximumYear,
+    required Function(DateTime date) onSelected,
+  }) {
+    final OverlayPortalController tooltipController = OverlayPortalController();
+    final link = LayerLink();
+    List<int> years = List.generate(maximumYear - minimumYear + 1, (index) => minimumYear + index);
+    return CompositedTransformTarget(
+      link: link,
+      child: OverlayPortal(
+        controller: tooltipController,
+        overlayChildBuilder: (BuildContext context) {
+          return CompositedTransformFollower(
+            link: link,
+            targetAnchor: Alignment.bottomLeft,
+            child: Align(
+              alignment: AlignmentDirectional.topStart,
+              child: Card(
+                elevation: 5,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: DefaultTabController(
+                      length: 2,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: TabBar(
+                              tabAlignment: TabAlignment.center,
+                              dividerColor: Colors.transparent,
+                              indicatorColor: palette['secondary'],
+                              tabs: [
+                                buildRegularText('Month', fontSize: 16),
+                                buildRegularText('Year', fontSize: 16),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Expanded(
+                            child: TabBarView(
+                              children: [
+                                // Month Grid View
+                                GridView.count(
+                                  crossAxisCount: 3,
+                                  childAspectRatio: 1.75,
+                                  children: List.generate(
+                                      _months.length,
+                                      (index) => InkWell(
+                                            borderRadius: BorderRadius.circular(10),
+                                            onTap: () {
+                                              onSelected(
+                                                  DateTime(selectedMonthYear.year, index + 1));
+                                            },
+                                            child: Container(
+                                              alignment: Alignment.center,
+                                              decoration: BoxDecoration(
+                                                  color: (index + 1) == selectedMonthYear.month
+                                                      ? palette['primary']
+                                                      : null,
+                                                  borderRadius: BorderRadius.circular(10)),
+                                              child: buildRegularText(_months[index],
+                                                  fontSize: 16,
+                                                  color: (index + 1) == selectedMonthYear.month
+                                                      ? Colors.white
+                                                      : palette['dark']),
+                                            ),
+                                          )),
+                                ),
+                                // Year Grid View
+                                GridView.count(
+                                  crossAxisCount: 3,
+                                  childAspectRatio: 1.75,
+                                  children: List.generate(
+                                      years.length,
+                                      (index) => InkWell(
+                                            borderRadius: BorderRadius.circular(10),
+                                            onTap: () {
+                                              onSelected(
+                                                  DateTime(years[index], selectedMonthYear.month));
+                                            },
+                                            child: Container(
+                                              alignment: Alignment.center,
+                                              decoration: BoxDecoration(
+                                                  color: years[index] == selectedMonthYear.year
+                                                      ? palette['primary']
+                                                      : null,
+                                                  borderRadius: BorderRadius.circular(10)),
+                                              child: buildRegularText(years[index].toString(),
+                                                  fontSize: 16,
+                                                  color: years[index] == selectedMonthYear.year
+                                                      ? Colors.white
+                                                      : palette['dark']),
+                                            ),
+                                          )),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      )),
+                ),
+              ),
+            ),
+          );
+        },
+        child: StatefulBuilder(builder: (_, setState) {
+          return InkWell(
+            borderRadius: BorderRadius.circular(5),
+            onTap: () {
+              setState(() {
+                tooltipController.toggle();
+              });
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                buildHeading3Text(
+                    '${_months[selectedMonthYear.month - 1]} ${selectedMonthYear.year}'),
+                const SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  tooltipController.isShowing
+                      ? Icons.arrow_drop_up_rounded
+                      : Icons.arrow_drop_down_rounded,
+                  size: 24,
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
   void _changeMonth(bool isNext) {
     int newMonth = _currentDate.month + (isNext ? 1 : -1);
+    int year = _currentDate.year;
     if (newMonth == 0) {
-      if (_selectedYear == _minimumYear) {
+      if (year == _minimumYear) {
         return;
       }
-      _selectedYear--;
-      _currentDate = DateTime(_selectedYear, 12, 1);
+      _currentDate = DateTime(_currentDate.year - 1, 12, 1);
     } else if (newMonth == 13) {
-      if (_selectedYear == _maximumYear) {
+      if (year == _maximumYear) {
         return;
       }
-      _selectedYear++;
-      _currentDate = DateTime(_selectedYear, 1, 1);
+      _currentDate = DateTime(_currentDate.year + 1, 1, 1);
     } else {
-      _currentDate = DateTime(_selectedYear, newMonth, 1);
+      _currentDate = DateTime(_currentDate.year, newMonth, 1);
     }
     setState(() {});
   }
 
-  GridView _buildCalendarGrid() {
+  Widget _buildCalendarGrid() {
     // Get the day of the first day of a month
     // Ex: July 1, 2024 would result to 1 and then add 1 to make it 2
     // The Sunday will be the first day of the week so it would be 1
     final firstDayOfMonthWeekday = _currentDate.weekday == 7 ? 1 : _currentDate.weekday + 1;
 
     // Get the total days in a month
-    final daysInMonth = DateTime(_selectedYear, _currentDate.month + 1, 0).day;
+    final daysInMonth = DateTime(_currentDate.year, _currentDate.month + 1, 0).day;
 
     // Calculate the total number of cells needed (including padding days)
     int totalCells = (firstDayOfMonthWeekday - 1) +
         daysInMonth +
         (7 - ((firstDayOfMonthWeekday + daysInMonth - 1) % 7));
 
-    return GridView.count(
-      crossAxisCount: 7,
-      mainAxisSpacing: 20,
-      children: List.generate(totalCells, (index) {
-        int day;
-
-        if (index < firstDayOfMonthWeekday - 1) {
-          // Previous month's day
-          final previousMonth = DateTime(_selectedYear, _currentDate.month, 0);
-          final previousMonthDays = previousMonth.day;
-          day = previousMonthDays - (firstDayOfMonthWeekday - 2 - index);
-          return _buildDayCell(DateTime(_selectedYear, _currentDate.month - 1, day),
-              isPreviousMonth: true);
-        } else if (index >= firstDayOfMonthWeekday - 1 + daysInMonth) {
-          // Next month's day
-          day = index - (firstDayOfMonthWeekday - 2 + daysInMonth);
-          return _buildDayCell(DateTime(_selectedYear, _currentDate.month + 1, day),
-              isNextMonth: true);
-        } else {
-          // Current month's day
-          day = index - (firstDayOfMonthWeekday - 2);
-          return _buildDayCell(DateTime(_selectedYear, _currentDate.month, day));
+    return GestureDetector(
+      onHorizontalDragEnd: (details) {
+        if (details.primaryVelocity! < 0) {
+          // Swiped left
+          // Next
+          _changeMonth(true);
+        } else if (details.primaryVelocity! > 0) {
+          // Swiped right
+          // Previous
+          _changeMonth(false);
         }
-      }),
+      },
+      child: GridView.count(
+        crossAxisCount: 7,
+        mainAxisSpacing: 20,
+        children: List.generate(totalCells, (index) {
+          int day;
+
+          if (index < firstDayOfMonthWeekday - 1) {
+            // Previous month's day
+            final previousMonth = DateTime(_currentDate.year, _currentDate.month, 0);
+            final previousMonthDays = previousMonth.day;
+            day = previousMonthDays - (firstDayOfMonthWeekday - 2 - index);
+            return _buildDayCell(DateTime(_currentDate.year, _currentDate.month - 1, day),
+                isPreviousMonth: true);
+          } else if (index >= firstDayOfMonthWeekday - 1 + daysInMonth) {
+            // Next month's day
+            day = index - (firstDayOfMonthWeekday - 2 + daysInMonth);
+            return _buildDayCell(DateTime(_currentDate.year, _currentDate.month + 1, day),
+                isNextMonth: true);
+          } else {
+            // Current month's day
+            day = index - (firstDayOfMonthWeekday - 2);
+            return _buildDayCell(DateTime(_currentDate.year, _currentDate.month, day));
+          }
+        }),
+      ),
     );
   }
 
@@ -215,12 +343,13 @@ class _CalendarViewState extends State<CalendarView> {
     Color? textColor;
     if (isPreviousMonth || isNextMonth) {
       textColor = palette['faded'];
-    } else if (isInRecord && !isCurrentDate) {
+    } else if (isInRecord) {
       textColor = Colors.white;
     } else {
       textColor = palette['dark'];
     }
-    return GestureDetector(
+    return InkWell(
+      borderRadius: BorderRadius.circular(180),
       onTap: () async {
         if (isFuture) {
           return;
@@ -239,9 +368,7 @@ class _CalendarViewState extends State<CalendarView> {
             decoration: BoxDecoration(
               border: isCurrentDate ? Border.all(color: palette['secondary']!, width: 3) : null,
               borderRadius: BorderRadius.circular(180),
-              color: isInRecord && !isCurrentDate && !(isPreviousMonth || isNextMonth)
-                  ? palette['primary']
-                  : null,
+              color: isInRecord && !(isPreviousMonth || isNextMonth) ? palette['primary'] : null,
             ),
             child: buildRegularText(date.day.toString(), color: textColor, fontSize: 18)),
       ),
